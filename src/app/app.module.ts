61956550e81aac498d03bc586759feb5ea12f1fc
josafat <josa221001@gmail.com>
2023-12-09 20:44:16 -0600

    updt: importacion de modulo de ciclo escolar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { AuthInterceptor } from './helpers/auth.interceptor';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CiclosEscolarresModule } from './ciclos-escolares/ciclos-escolarres.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HomeComponent } from './pages/home/home.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CiclosEscolarresModule
   ],
   providers: [
     CookieService,
