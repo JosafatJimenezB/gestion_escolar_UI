c7e80c6a79fcd6f55dff4d0dca85949a45babd8a
josafat <josa221001@gmail.com>
2023-12-09 20:43:54 -0600

    add: modulo para ciclos escolares

---
+++ src/app/ciclos-escolares/components/ciclo-escolar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo-escolar',
+  templateUrl: './ciclo-escolar.component.html'
+})
+
+export class NameComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
